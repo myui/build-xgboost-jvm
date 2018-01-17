604fd3d629b3a34010524713b31df5080c3e63f2
Makoto Yui <myui@apache.org>
2018-01-18 03:59:00 +0900

    Fixed build error for missing pthread

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,12 +4,13 @@ cd xgboost/
 
 cat make/config.mk | sed -e 's/USE_OPENMP = 1/USE_OPENMP = 0/' > config.mk
 sed -i -e 's/LINK_LIBRARIES dmlccore/LINK_LIBRARIES dmlc/' CMakeLists.txt
-sed -i -e 's/find_package(OpenMP)/#find_package(OpenMP)/' CMakeLists.txt
-sed -i -e 's/${CMAKE_CXX_FLAGS} -funroll-loops/${CMAKE_CXX_FLAGS} -funroll-loops -static-libgcc -static-libstdc++ -fvisibility=hidden/' CMakeLists.txt
+sed -i -e 's/find_package(OpenMP)/find_package(Threads REQUIRED)/' CMakeLists.txt
+sed -i -e 's/${CMAKE_CXX_FLAGS} -funroll-loops/${CMAKE_CXX_FLAGS} -funroll-loops -pthread -static-libgcc -static-libstdc++ -fvisibility=hidden/' CMakeLists.txt
 
 cd dmlc-core/
 # https://github.com/dmlc/dmlc-core/commit/2777ad99d823848cbce6354688b397d519f7b810
 git checkout ${DMLC_CORE_COMMIT_HASH}
+sed -i -e 's/dmlccore_option(USE_OPENMP "Build with OpenMP" ON)/dmlccore_option(USE_OPENMP "Build with OpenMP" OFF)/' CMakeLists.txt
 
 cd ../jvm-packages
 sed -i -e 's/"USE_OPENMP": "ON"/"USE_OPENMP": "OFF"/' create_jni.py
