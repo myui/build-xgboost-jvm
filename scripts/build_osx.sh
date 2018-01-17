105650e7eb99dd9850373dae13747b0dbb203a8b
Makoto Yui <myui@apache.org>
2018-01-18 05:03:49 +0900

    Fixed CI scripts

---
+++ scripts/build_osx.sh
@@ -0,0 +1,23 @@
+git clone --recursive --branch v${XGBOOST_VERSION} --depth 1 --single-branch https://github.com/dmlc/xgboost
+
+cd xgboost/
+
+cat make/config.mk | sed -e 's/USE_OPENMP = 1/USE_OPENMP = 0/' > config.mk
+sed -i -e 's/LINK_LIBRARIES dmlccore/LINK_LIBRARIES dmlc/' CMakeLists.txt
+sed -i -e 's/find_package(OpenMP)/find_package(Threads REQUIRED)/' CMakeLists.txt
+sed -i -e 's/${CMAKE_CXX_FLAGS} -funroll-loops/${CMAKE_CXX_FLAGS} -funroll-loops -pthread' CMakeLists.txt
+
+cd dmlc-core/
+# https://github.com/dmlc/dmlc-core/commit/2777ad99d823848cbce6354688b397d519f7b810
+git checkout ${DMLC_CORE_COMMIT_HASH}
+sed -i -e 's/dmlccore_option(USE_OPENMP "Build with OpenMP" ON)/dmlccore_option(USE_OPENMP "Build with OpenMP" OFF)/' CMakeLists.txt
+
+cd ../jvm-packages
+sed -i -e 's/"USE_OPENMP": "ON"/"USE_OPENMP": "OFF"/' create_jni.py
+
+export USE_OPENMP=0
+./create_jni.py
+
+mvn -pl :xgboost4j package
+
+mv xgboost4j/target/xgboost4j-$XGBOOST_VERSION.jar $TRAVIS_BUILD_DIR/xgboost4j-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
