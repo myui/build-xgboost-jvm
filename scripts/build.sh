8f7f29c561ae94f79d88d2cc8ea1115bf9875c85
Makoto Yui <myui@apache.org>
2018-01-17 18:01:35 +0900

    Initial import

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+git clone --recursive --branch v${XGBOOST_VERSION} --depth 1 --single-branch https://github.com/dmlc/xgboost
+
+cd xgboost/
+
+cat make/config.mk | sed -e 's/USE_OPENMP = 1/USE_OPENMP = 0/' > config.mk
+sed -i '' -e 's/LINK_LIBRARIES dmlccore/LINK_LIBRARIES dmlc/' CMakeLists.txt
+sed -i '' -e 's/find_package(OpenMP)/#find_package(OpenMP)/' CMakeLists.txt
+sed -i '' -e 's/${CMAKE_CXX_FLAGS} -funroll-loops/${CMAKE_CXX_FLAGS} -funroll-loops -static-libgcc -static-libstdc++ -fvisibility=hidden/' CMakeLists.txt
+
+cd dmlc-core/
+# https://github.com/dmlc/dmlc-core/commit/2777ad99d823848cbce6354688b397d519f7b810
+git checkout ${DMLC_CORE_COMMIT_HASH}
+
+cd ../jvm-packages
+sed -i '' -e 's/"USE_OPENMP": "ON"/"USE_OPENMP": "OFF"/' create_jni.py 
+
+export USE_OPENMP=0
+./create_jni.py
+ldd xgboost4j/src/main/resources/lib/libxgboost4j.dylib
+
+mvn -pl :xgboost4j package
+
+mv xgboost4j/target/xgboost4j-$XGBOOST_VERSION.jar $TRAVIS_BUILD_DIR/xgboost4j-$XGBOOST_VERSION-$TRAVIS_OS_NAME.jar
