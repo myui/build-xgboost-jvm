7e5cee0d520f747154b4527ae0c9e19835b2cced
Makoto Yui <myui@apache.org>
2019-10-30 00:13:31 +0900

    Added missing fields

--- src/LabeledPoint.java
+++ src/LabeledPoint.java
@@ -1,14 +1,35 @@
+/*
+ Copyright (c) 2014 by Contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package ml.dmlc.xgboost4j;
 
 import java.io.Serializable;
 
-public final class LabeledPoint implements Serializable {
+/**
+ * Labeled training data point.
+ */
+public class LabeledPoint implements Serializable {
   private static final long serialVersionUID = -8258523529185099548L;
 
-  private final float label;
-  private final int[] indices;
-  private final float[] values;
+  private float label;
+  private int[] indices;
+  private float[] values;
   private float weight = 1.f;
+  private int group = -1;
+  private float baseMargin = Float.NaN;
 
   public LabeledPoint(float label, int[] indices, float[] values) {
     if (indices == null || values == null) {
@@ -37,6 +58,14 @@ public final class LabeledPoint implements Serializable {
 
   public float weight() {
     return weight;
+  }
+
+  public int group() {
+    return group;
+  }
+
+  public float baseMargin() {
+    return baseMargin;
   }
 
 }
